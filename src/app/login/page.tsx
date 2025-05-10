0298095a12e489f4b8f672e9d9c738fce5b8e4ac
kinglove58 <obafemielijahsunday@gmail.com>
2025-05-10 12:02:59 +0100

    feat: enhance Authentication page layout with updated styles and testimonial section

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,15 +6,25 @@ import Logo from "@/components/logo";
 const Authentication = () => {
   return (
     <main className="h-screen grid grid-cols-2">
-      <div className="relative p-10 bg-muted flex flex-col w-full h-full">
+      <div className="relative p-10 bg-muted flex flex-col w-full h-full text-primary-foreground">
         <Image
           src={AuthImage}
           alt="wallpaper image for login"
           fill
           className="object-cover"
         />
-        <div className="relative items-center flex z-20">
+        <div className="relative items-center flex z-20 ">
           <Logo />
+        </div>
+        <div className="z-20 relative mt-auto">
+          <blockquote className="space-y-2">
+            <p className="text-lg">
+              &ldquo;Pictoria AI is a game changer for me. I have been able to
+              generate high quality professional headshots within minutes. It
+              has saved me countless hours of work and cost as well.&rdquo;{" "}
+            </p>
+            <footer className="text-sm">David S.</footer>
+          </blockquote>
         </div>
       </div>
       <div className="flex items-center justify-center w-full h-full">
