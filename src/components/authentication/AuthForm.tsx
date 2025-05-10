4d9c1b06e48c8f88926ea2ab877415af7f2a8ea1
kinglove58 <obafemielijahsunday@gmail.com>
2025-05-10 12:43:46 +0100

    feat: update AuthForm component to improve user prompts and structure

--- src/components/authentication/AuthForm.tsx
+++ src/components/authentication/AuthForm.tsx
@@ -5,13 +5,25 @@ const AuthForm = () => {
   const [mode, setMode] = useState("login");
   return (
     <div className="space-y-6">
-      <div className="space-y-2 tracking-tight ">
-        {mode === "reset"
-          ? "Please reset your password"
-          : mode === "login"
-          ? "Please login to your account"
-          : "Singup for your account"}
+      <div className="space-y-2 tracking-tight">
+        <h1 className="font-semibold text-lg">
+          {" "}
+          {mode === "reset"
+            ? "Reset Password"
+            : mode === "login"
+            ? "Login"
+            : "Signup"}
+        </h1>
+        <p>
+          {" "}
+          {mode === "reset"
+            ? "Please reset your password here"
+            : mode === "login"
+            ? "please enter your email and password"
+            : "please input your correct details"}{" "}
+        </p>
       </div>
+      <div></div>
     </div>
   );
 };
