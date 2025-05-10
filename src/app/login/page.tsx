e7eef9008ca0cb8c6330e72f07009530a69a7291
kinglove58 <obafemielijahsunday@gmail.com>
2025-05-10 12:29:19 +0100

    feat: update Authentication component styles and testimonial text size

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,6 +7,8 @@ const Authentication = () => {
   return (
     <main className="h-screen grid grid-cols-2">
       <div className="relative p-10 bg-muted flex flex-col w-full h-full text-primary-foreground">
+        <div className="w-full h-[30%] bg-gradient-to-t from-transparent to-black/50 absolute top-0 left-0 z-10 " />
+        <div className="w-full h-[40%] bg-gradient-to-b from-transparent to-black/50 absolute bottom-0 left-0 z-10 " />
         <Image
           src={AuthImage}
           alt="wallpaper image for login"
@@ -18,17 +20,19 @@ const Authentication = () => {
         </div>
         <div className="z-20 relative mt-auto">
           <blockquote className="space-y-2">
-            <p className="text-lg">
+            <p className="text-sm">
               &ldquo;Pictoria AI is a game changer for me. I have been able to
               generate high quality professional headshots within minutes. It
               has saved me countless hours of work and cost as well.&rdquo;{" "}
             </p>
-            <footer className="text-sm">David S.</footer>
+            <footer className="text-xs">David S.</footer>
           </blockquote>
         </div>
       </div>
       <div className="flex items-center justify-center w-full h-full">
-        <div className="w-3/4">log in</div>
+        <div className="flex flex-col justify-center items-center p-8">
+          log in
+        </div>
       </div>
     </main>
   );
