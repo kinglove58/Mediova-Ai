92a527a276c324b3188a299399ba845fe2b90e7a
kinglove58 <obafemielijahsunday@gmail.com>
2025-05-16 13:13:34 +0100

    refactor: update form schema in Configuration component to include model, prompt, guidance, and num_outputs

--- src/components/image-generation/Configuration.tsx
+++ src/components/image-generation/Configuration.tsx
@@ -16,7 +16,19 @@ import {
 import { Input } from "@/components/ui/input";
 
 const formSchema = z.object({
-  username: z.string().min(2, { message: "this" }).max(50),
+  model: z.string({
+    required_error: "model is required",
+  }),
+  prompt: z.string({
+    required_error: "prompt is required",
+  }),
+  guidance: z.number({
+    required_error: "guidance scale is required",
+  }),
+  num_outputs: z
+    .number()
+    .min(1, { message: "Number of output should be atleast 1" })
+    .max(4, { message: "Number of output must be less than 4" }),
 });
 const Configuration = () => {
   const form = useForm<z.infer<typeof formSchema>>({
@@ -26,13 +38,34 @@ const Configuration = () => {
     },
   });
 
-  // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     console.log(values);
   }
-  return <div>Configuration</div>;
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 };
 
 export default Configuration;
