15c81366ce7d9c3353a95d4248061db837f03806
kinglove58 <obafemielijahsunday@gmail.com>
2025-05-16 13:05:10 +0100

    refactor: improve structure and add form handling to Configuration component

--- src/app/(dashboard)/image-generation/page.tsx
+++ src/app/(dashboard)/image-generation/page.tsx
@@ -1,13 +1,15 @@
-import Configuration from '@/components/image-generation/Configuration'
-import React from 'react'
+import Configuration from "@/components/image-generation/Configuration";
+import React from "react";
 
 const ImageGenerationPage = () => {
   return (
-    <section className='grid grid-cols-3 gap-4 container mx-auto overflow-hidden'>
-      <Configuration/>
-      <div>output</div>
+    <section className="grid grid-cols-3 gap-4 container mx-auto overflow-hidden">
+      <Configuration />
+      <div className="col-span-2 p-4 flex justify-center rounded-xl items-center">
+        output
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default ImageGenerationPage
+export default ImageGenerationPage;
--- src/components/image-generation/Configuration.tsx
+++ src/components/image-generation/Configuration.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
+const formSchema = z.object({
+  username: z.string().min(2, { message: "this" }).max(50),
+});
 const Configuration = () => {
-  return (
-    <div>Configuration</div>
-  )
-}
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
 
-export default Configuration
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+  return <div>Configuration</div>;
+};
+
+export default Configuration;
