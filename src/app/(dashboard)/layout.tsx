7b7953c78e452d847a226a901086c9dd66ab1bce
kinglove58 <obafemielijahsunday@gmail.com>
2025-05-19 09:36:01 +0100

    refactor: adjust layout structure in DashboardLayout for improved sidebar trigger alignment

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -14,10 +14,10 @@ export default function DashboardLayout({
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <SidebarTrigger className="-ml-1" />
-        <main className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          {children}
-        </main>{" "}
+        <div className="flex items-center my-4 w-fit gap-2">
+          <SidebarTrigger className="-ml-1" />
+        </div>
+        <main className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</main>{" "}
       </SidebarInset>
     </SidebarProvider>
   );
