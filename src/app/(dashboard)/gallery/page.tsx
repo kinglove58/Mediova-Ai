1ded645d8d7966b1bfb87826429cc66f38fcdc78
kinglove58 <obafemielijahsunday@gmail.com>
2025-05-23 18:26:17 +0100

    feat: implement GalleryPage and GalleryComponent for displaying generated images

--- src/app/(dashboard)/gallery/page.tsx
+++ src/app/(dashboard)/gallery/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import GalleryComponent from "@/components/gallery/GalleryComponent";
+import React from "react";
 
 const GalleryPage = () => {
   return (
-    <div>GalleryPage</div>
-  )
-}
+    <section className="container mx-auto">
+      <h1 className="text-3xl mb-2 font-semibold">Gallery</h1>
+      <p className="text-muted-foreground mb-6">
+        Here you can see all the images you have generated. Click on an image to
+        view detail
+      </p>
+      <GalleryComponent />
+    </section>
+  );
+};
 
-export default GalleryPage
+export default GalleryPage;
