e670924ef9d193317d37e5c21c11c8c48a3ca688
kinglove58 <obafemielijahsunday@gmail.com>
2025-05-27 17:30:20 +0100

    fix: correct image length check condition in GalleryComponent

--- src/components/gallery/GalleryComponent.tsx
+++ src/components/gallery/GalleryComponent.tsx
@@ -9,17 +9,21 @@ interface GalleryProps {
   images: ImageProps[];
 }
 
-const GalleryComponent = ({images}:GalleryProps) => {
+const GalleryComponent = ({ images }: GalleryProps) => {
   console.log(images);
-  
-  if(images.length = 0){
-    return <div className="flex justify-center h-[50vh] items-center text-muted-foreground">No images found</div>
+
+  if ((images.length = 0)) {
+    return (
+      <div className="flex justify-center h-[50vh] items-center text-muted-foreground">
+        No images found
+      </div>
+    );
   }
   return (
     <section className="container mx-auto py-8">
       <div className="columns-4 "></div>
     </section>
-  )
+  );
 };
 
 export default GalleryComponent;
