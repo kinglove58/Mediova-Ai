45d7d8e176fc4c2e4ad69b383704784278e39beb
kinglove58 <obafemielijahsunday@gmail.com>
2025-05-28 09:59:35 +0100

    fix: integrate ScrollArea in ImageDialog for improved layout and scrolling of image details

--- src/components/gallery/ImageDialog.tsx
+++ src/components/gallery/ImageDialog.tsx
@@ -11,6 +11,7 @@ import {
 import Image from "next/image";
 import { Button } from "../ui/button";
 import { Download, Trash } from "lucide-react";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { Badge } from "../ui/badge";
 
 interface ImageDialogProps {
@@ -23,94 +24,96 @@ const ImageDialog = ({ image, onclose }: ImageDialogProps) => {
       <SheetContent className="max-w-full sm:max-w-xl w-full">
         <SheetHeader>
           <SheetTitle className="text-2xl w-full">Image Details</SheetTitle>
-          <div className="relative w-fit h-fit">
-            <Image
-              src={image.url ?? ""}
-              alt={image.prompt ?? "Generated image"}
-              width={image.width ?? 1}
-              height={image.height ?? 1}
-              className="object-cover rounded"
-            />{" "}
-            <div className="flex absolute bottom-4 right-4 gap-4">
-              <Button className="w-fit">
-                <Download className="w-4 h-4 mr-2" /> Download
-              </Button>{" "}
-              <Button className="w-fit" variant={"destructive"}>
-                <Trash className="w-4 h-4" /> Trash
-              </Button>
+          <ScrollArea className="h-72 w-48 rounded-md border">
+            <div className="relative w-fit h-fit">
+              <Image
+                src={image.url ?? ""}
+                alt={image.prompt ?? "Generated image"}
+                width={image.width ?? 1}
+                height={image.height ?? 1}
+                className="object-cover rounded"
+              />{" "}
+              <div className="flex absolute bottom-4 right-4 gap-4">
+                <Button className="w-fit">
+                  <Download className="w-4 h-4 mr-2" /> Download
+                </Button>{" "}
+                <Button className="w-fit" variant={"destructive"}>
+                  <Trash className="w-4 h-4" /> Trash
+                </Button>
+              </div>
             </div>
-          </div>
-          <hr className="inline-block w-full border-primary/30 mb-2" />
-          <p className="flex flex-col text-primary/90">
-            <span className="text-primary text-xl font-semibold">Prompt</span>
-            {image.prompt}
-          </p>
-          <hr className="inline-block w-full border-primary/30 my-3" />
-          <div className="flex flex-wrap gap-3">
-            <Badge
-              variant={"secondary"}
-              className="rounded-full border border-primary/30 px-4 py-2 text-sm font-normal"
-            >
-              <span className="uppercase mr-2 text-primary font-semibold ">
-                Model-Id:
-              </span>{" "}
-              {image.model}
-            </Badge>
-            <Badge
-              variant={"secondary"}
-              className="rounded-full border border-primary/30 px-4 py-2 text-sm font-normal"
-            >
-              <span className="uppercase mr-2 text-primary font-semibold ">
-                Aspect_ratio:
-              </span>{" "}
-              {image.aspect_ratio}
-            </Badge>
-            <Badge
-              variant={"secondary"}
-              className="rounded-full border border-primary/30 px-4 py-2 text-sm font-normal"
-            >
-              <span className="uppercase mr-2 text-primary font-semibold ">
-                Dimension
-              </span>{" "}
-              {image.width} X {image.height}
-            </Badge>
-            <Badge
-              variant={"secondary"}
-              className="rounded-full border border-primary/30 px-4 py-2 text-sm font-normal"
-            >
-              <span className="uppercase mr-2 text-primary font-semibold ">
-                guidance:
-              </span>{" "}
-              {image.guidance}
-            </Badge>
-            <Badge
-              variant={"secondary"}
-              className="rounded-full border border-primary/30 px-4 py-2 text-sm font-normal"
-            >
-              <span className="uppercase mr-2 text-primary font-semibold ">
-                inference_steps:
-              </span>{" "}
-              {image.num_inference_steps}
-            </Badge>{" "}
-            <Badge
-              variant={"secondary"}
-              className="rounded-full border border-primary/30 px-4 py-2 text-sm font-normal"
-            >
-              <span className="uppercase mr-2 text-primary font-semibold ">
-                output_format:
-              </span>{" "}
-              {image.output_format}
-            </Badge>{" "}
-            <Badge
-              variant={"secondary"}
-              className="rounded-full border border-primary/30 px-4 py-2 text-sm font-normal"
-            >
-              <span className="uppercase mr-2 text-primary font-semibold ">
-                Created At:
-              </span>{" "}
-              {new Date(image.created_at).toLocaleDateString()}
-            </Badge>
-          </div>
+            <hr className="inline-block w-full border-primary/30 mb-2" />
+            <p className="flex flex-col text-primary/90">
+              <span className="text-primary text-xl font-semibold">Prompt</span>
+              {image.prompt}
+            </p>
+            <hr className="inline-block w-full border-primary/30 my-3" />
+            <div className="flex flex-wrap gap-3">
+              <Badge
+                variant={"secondary"}
+                className="rounded-full border border-primary/30 px-4 py-2 text-sm font-normal"
+              >
+                <span className="uppercase mr-2 text-primary font-semibold ">
+                  Model-Id:
+                </span>{" "}
+                {image.model}
+              </Badge>
+              <Badge
+                variant={"secondary"}
+                className="rounded-full border border-primary/30 px-4 py-2 text-sm font-normal"
+              >
+                <span className="uppercase mr-2 text-primary font-semibold ">
+                  Aspect_ratio:
+                </span>{" "}
+                {image.aspect_ratio}
+              </Badge>
+              <Badge
+                variant={"secondary"}
+                className="rounded-full border border-primary/30 px-4 py-2 text-sm font-normal"
+              >
+                <span className="uppercase mr-2 text-primary font-semibold ">
+                  Dimension
+                </span>{" "}
+                {image.width} X {image.height}
+              </Badge>
+              <Badge
+                variant={"secondary"}
+                className="rounded-full border border-primary/30 px-4 py-2 text-sm font-normal"
+              >
+                <span className="uppercase mr-2 text-primary font-semibold ">
+                  guidance:
+                </span>{" "}
+                {image.guidance}
+              </Badge>
+              <Badge
+                variant={"secondary"}
+                className="rounded-full border border-primary/30 px-4 py-2 text-sm font-normal"
+              >
+                <span className="uppercase mr-2 text-primary font-semibold ">
+                  inference_steps:
+                </span>{" "}
+                {image.num_inference_steps}
+              </Badge>{" "}
+              <Badge
+                variant={"secondary"}
+                className="rounded-full border border-primary/30 px-4 py-2 text-sm font-normal"
+              >
+                <span className="uppercase mr-2 text-primary font-semibold ">
+                  output_format:
+                </span>{" "}
+                {image.output_format}
+              </Badge>{" "}
+              <Badge
+                variant={"secondary"}
+                className="rounded-full border border-primary/30 px-4 py-2 text-sm font-normal"
+              >
+                <span className="uppercase mr-2 text-primary font-semibold ">
+                  Created At:
+                </span>{" "}
+                {new Date(image.created_at).toLocaleDateString()}
+              </Badge>
+            </div>
+          </ScrollArea>
         </SheetHeader>
       </SheetContent>
     </Sheet>
